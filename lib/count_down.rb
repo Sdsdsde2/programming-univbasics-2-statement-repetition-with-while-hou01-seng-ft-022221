8c2c99fa8dbb6d72ea5a7cc0cd4a518c1e0db82d
Shane R <imspammails@gmail.com>
2021-01-08 07:48:10 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,17 @@
 # Write your code here
+
+# Create timer
+countdown_timer = 10
+
+# Create the while loop
+while countdown_timer > 0 do
+  
+  # Break if countdown timer reaches 1
+  break if countdown_timer < 1
+  
+  "#{countdown_timer}"
+  countdown_timer -=
+end
+
+# Print "Happy New Year!"
+puts "Happy New Year!"
